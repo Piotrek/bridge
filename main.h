d08af2987da41195a340b4d495367fe4528f5c82
Piotrek Butryn <piotrek@wiewior2.(none)>
2010-09-14 15:15:57 +0200

    main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <vector>
+#include <set>
+
+#include "deal.h"
+#include "utils.h"
+#include "uct.h"
+
+#endif
